57b8f6ae3047e9a0380312f6674b0e4978b54f8c
AriusTXB <165741616+AriusTXB@users.noreply.github.com>
2024-04-15 01:15:33 +0700

    Create Ex1.java

---
+++ HW1/Ex1.java
@@ -0,0 +1,14 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println(
+                "Tran Xuan Bao" + '\t' + "23020332" + '\t' + "K68AI2" + '\t' + "AriusTXB" + '\t'
+                        + "Tranxuanbaohtka@gmail.com");
+        for (int i = 9; i > 1; i--) {
+            System.out.println(i + " bottles of beer on the wall," + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
